6abb32845548c6f9099bc00950016ab77f8f917b
George Oastler <goastler4@gmail.com>
2023-06-02 12:03:02 +0100

    add an empty contract

---
+++ contracts/empty/src/lib.rs
@@ -0,0 +1,25 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[ink::contract]
+pub mod contract {
+
+    #[ink(storage)]
+    pub struct Contract {
+
+    }
+
+    impl Contract {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {
+
+            }
+        }
+
+        // A function which makes use of the storage to ensure optimisation cannot eliminate it from storage entirely (not sure if this happens, but best to do this just in case).
+        #[ink(message)]
+        pub fn func(&self) {
+            
+        }
+    }
+}
